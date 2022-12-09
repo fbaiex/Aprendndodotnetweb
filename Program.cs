f08f14e21aae668657559b4dd948a5d816e01a80
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-12-09 11:17:38 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World 2!");
 app.MapPost("/", () => new {name = "Fabio Aiex", Age = 28});
-app.MapGet("/AddHeader", (HttpResponse response) => response.Headers.Add("Teste", "Fabio Aiex Alves"));
+app.MapGet("/AddHeader", (HttpResponse response) => {response.Headers.Add("Teste", "Fabio Aiex Alves");
+    return new {name = "Fabio Aiex", Age = 28};
+});
+
+app.MapPost("/saveproduct", (Product product) => {
+    return product.Code + " - " + product.Name;
+});
+
+app.MapGet("/getproduct", ([FromQuery] string dateStart, string dateEnd) => {
+    return dateStart + " - " + dateEnd;
+});
+
+app.MapGet("/getproduct/{code}", ([FromRoute]string code) => {
+    return code;
+});
 
 app.Run();
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
